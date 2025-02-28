1f746c83595fa80468a8334f907e38d8fb4e725d
Wheslley Rimar Bezerra <wheslley_rimar@hotmail.com>
2025-02-28 07:44:58 -0300

    feat: ensure ASCII Art is displayed for --help flag in any command

--- core/src/main/java/dev/buildcli/core/utils/BuildCLIService.java
+++ core/src/main/java/dev/buildcli/core/utils/BuildCLIService.java
@@ -7,6 +7,7 @@ import dev.buildcli.core.log.SystemOutLogger;
 import dev.buildcli.core.utils.tools.CLIInteractions;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.Attributes;
@@ -47,14 +48,26 @@ public class BuildCLIService {
     if (args.length == 0) {
       return false;
     }
+
+    if (Arrays.asList(args).contains("--help")) {
+      return true;
+    }
+
     Map<String, List<String>> commandAliases = Map.of(
             "p", List.of("p", "project"),
-            "about", List.of("a", "about")
+            "about", List.of("a", "about"),
+            "help", List.of("help", "h")
     );
+
     String mainCommand = args[0];
     if (matchesCommand(mainCommand, commandAliases.get("p"))) {
       return args.length > 1 && (args[1].equals("run") || (args.length > 2 && args[1].equals("i") && args[2].equals("-n")));
     }
+
+    if (matchesCommand(mainCommand, commandAliases.get("help"))) {
+      return true;
+    }
+
     return matchesCommand(mainCommand, commandAliases.get("about"));
   }
 
