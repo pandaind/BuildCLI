5b63241cce1e708a50c876d10cac11b043cb3423
Matheus Oliveira da Silva <99829531+omatheusmesmo@users.noreply.github.com>
2025-03-03 11:02:13 -0300

    fix: hotfix in install.bat script

--- install.bat
+++ install.bat
@@ -42,7 +42,7 @@ if %errorlevel% neq 0 (
 )
 
 echo Building and packaging the project...
-mvn clean package || (
+mvn clean package -DskipTests || (
     echo Maven build failed.
     pause
     exit /b 1
@@ -61,7 +61,7 @@ copy cli\target\buildcli.jar "%USERPROFILE%\bin\buildcli.jar" >nul
 echo Creating buildcli.bat shortcut...
 (
     echo @echo off
-    echo java -jar "%%USERPROFILE%%\bin\buildcli.jar" %%*
+    echo java --enable-preview --add-modules jdk.incubator.vector -jar "%%USERPROFILE%%\bin\buildcli.jar" %%*
 ) > "%USERPROFILE%\bin\buildcli.bat"
 
 echo Ensuring %USERPROFILE%\bin is in the PATH...
